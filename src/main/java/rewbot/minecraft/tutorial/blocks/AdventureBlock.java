6479d9dc301f9e5c32f236d004aa292d27d3b87f
Andrew Shepherd <ashepherd@journaltech.com>
2016-05-19 22:25:12 -0600

    Drop random number of topaz stones on topaz ore break

--- src/main/java/rewbot/minecraft/tutorial/blocks/AdventureBlock.java
+++ src/main/java/rewbot/minecraft/tutorial/blocks/AdventureBlock.java
@@ -2,8 +2,13 @@ package rewbot.minecraft.tutorial.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import rewbot.minecraft.tutorial.MmaGlobal;
+import rewbot.minecraft.tutorial.items.AdventureItems;
+
+import java.util.Random;
 
 /**
  * Created by ashepherd on 5/12/2016.
@@ -17,5 +22,13 @@ public class AdventureBlock extends Block {
         this.setHardness(hardness);
         this.setResistance(resistance);
         setCreativeTab(MmaGlobal.mmaCreativeTab);
+    }
+
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return this == AdventureBlocks.topazOre ? AdventureItems.topazStone : Item.getItemFromBlock(this);
+    }
+
+    public int quantityDropped(Random rand) {
+        return this == AdventureBlocks.topazOre ? 1 + rand.nextInt(5) : 1;
     }
 }
