de6304c8306158a85aa15787153cb83d9e43a3bd
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-02 21:26:58 -0600

    Generate new ores in world
    *Topaz and copper ore randomly generated in new world

--- src/main/java/rewbot/minecraft/tutorial/proxy/CommonProxy.java
+++ src/main/java/rewbot/minecraft/tutorial/proxy/CommonProxy.java
@@ -11,6 +11,7 @@ import rewbot.minecraft.tutorial.crafting.MmaSmelting;
 import rewbot.minecraft.tutorial.handler.MmaDropHandler;
 import rewbot.minecraft.tutorial.handler.MmaFuelHandler;
 import rewbot.minecraft.tutorial.items.AdventureItems;
+import rewbot.minecraft.tutorial.world.MmaWorldGen;
 
 /**
  * Created by ashepherd on 5/12/2016.
@@ -27,6 +28,7 @@ public class CommonProxy {
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new MmaDropHandler());
         GameRegistry.registerFuelHandler(new MmaFuelHandler());
+        GameRegistry.registerWorldGenerator(new MmaWorldGen(), 0);
     }
 
     public void postInit(FMLPostInitializationEvent postEvent) {
---
+++ src/main/java/rewbot/minecraft/tutorial/world/MmaWorldGen.java
@@ -0,0 +1,58 @@
+package rewbot.minecraft.tutorial.world;
+
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraftforge.fml.common.IWorldGenerator;
+import rewbot.minecraft.tutorial.blocks.AdventureBlocks;
+
+import java.util.Random;
+
+/**
+ * Created by ashepherd on 5/31/2016.
+ */
+public class MmaWorldGen implements IWorldGenerator {
+    //Ores
+    private WorldGenerator copperOre;
+
+    //Gems
+    private WorldGenerator topazOre;
+
+    public MmaWorldGen() {
+        copperOre = new MmaWorldGenMinable(AdventureBlocks.copperOre.getDefaultState(), 8);
+        topazOre = new MmaWorldGenMinable(AdventureBlocks.topazOre.getDefaultState(), 8);
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+        switch(world.provider.getDimensionId()) {
+            case 0: //Overworld
+                //Ores
+                runGenerator(copperOre, world, random, chunkX, chunkZ, 50, 0, 228);
+                //Gems
+                runGenerator(topazOre, world, random, chunkX, chunkZ, 10, 0, 128);
+                break;
+            case -1: //nether
+                //Nether Ores
+                break;
+            case 1: //The End
+                //End Ores
+                break;
+        }
+    }
+
+    private void runGenerator (WorldGenerator generator, World world, Random rand, int chunkX, int chunkZ, int chanceToSpawn, int minHeight, int maxHeight) {
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight) {
+            throw new IllegalArgumentException("Minimum or Maximum height out of bounds");
+        }
+
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chanceToSpawn; i++) {
+            int x = chunkX * 16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunkZ * 16 + rand.nextInt(16);
+            generator.generate(world, rand, new BlockPos(x, y, z));
+        }
+    }
+}
