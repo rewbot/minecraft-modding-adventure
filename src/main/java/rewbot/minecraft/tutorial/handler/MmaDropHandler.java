110a71563b31db851cb567478a171dcf9691524f
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-05 22:31:46 -0600

    Cleanup MmaDropHandler

--- src/main/java/rewbot/minecraft/tutorial/handler/MmaDropHandler.java
+++ src/main/java/rewbot/minecraft/tutorial/handler/MmaDropHandler.java
@@ -2,8 +2,6 @@ package rewbot.minecraft.tutorial.handler;
 
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockPlanks;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.passive.EntityChicken;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
@@ -11,9 +9,6 @@ import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import rewbot.minecraft.tutorial.items.MmaItems;
 import rewbot.minecraft.tutorial.items.tools.MmaResinHarvester;
-import rewbot.minecraft.tutorial.items.tools.MmaShaleMattock;
-
-import java.util.Random;
 
 /**
  * Created by ashepherd on 5/30/2016.
@@ -22,23 +17,14 @@ public class MmaDropHandler {
 
     @SubscribeEvent
     public void addEntityDrop(LivingDropsEvent event) {
-        if (event.entity instanceof EntityChicken) {
-            ItemStack copperIngot = new ItemStack(MmaItems.copperIngot, 1);
-            event.drops.add(new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, copperIngot));
-        }
+//        if (event.entity instanceof EntityChicken) {
+//            ItemStack copperIngot = new ItemStack(MmaItems.copperIngot, 1);
+//            event.drops.add(new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, copperIngot));
+//        }
     }
 
     @SubscribeEvent
     public void addBlockDrop(BlockEvent.HarvestDropsEvent event) {
-        if (event.state.getBlock() == Blocks.gravel) {
-            Random rand = new Random();
-            ItemStack holding = event.harvester.inventory.getStackInSlot(event.harvester.inventory.currentItem);
-            if(holding != null && holding.getItem() instanceof MmaShaleMattock) {
-                event.drops.clear();
-                event.drops.add(new ItemStack(MmaItems.viscinousGel, rand.nextInt(4)));
-            }
-        }
-
         if (event.state.getBlock() == Blocks.log) {
             if (event.state.getValue(BlockOldLog.VARIANT) == BlockPlanks.EnumType.SPRUCE) {
                 ItemStack holding = event.harvester.inventory.getStackInSlot(event.harvester.inventory.currentItem);
