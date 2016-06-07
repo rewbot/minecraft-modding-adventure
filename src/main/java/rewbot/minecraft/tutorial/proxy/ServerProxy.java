1c6ce91704a459a21b8e305abdf63454b448fc55
Andrew Shepherd <ashepherd@journaltech.com>
2016-06-07 14:37:12 -0600

    Add server proxy

---
+++ src/main/java/rewbot/minecraft/tutorial/proxy/ServerProxy.java
@@ -0,0 +1,25 @@
+package rewbot.minecraft.tutorial.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by ashepherd on 6/7/2016.
+ */
+public class ServerProxy extends CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent preEvent) {
+
+        super.preInit(preEvent);
+    }
+
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    public void postInit(FMLPostInitializationEvent postEvent) {
+
+        super.postInit(postEvent);
+    }
+}
